ece3b54615abbb2a1c6ef10f667f9d86a65fdb83
olivia <opierez@gmail.com>
2023-01-10 18:13:09 -0500

    commit tree 128ec52456d245cbc055827d85fbefd6297389e7

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,85 @@
 # your code goes here
+require 'pry'
+
+class Person
+
+    attr_accessor :bank_account, :happiness, :hygiene
+    attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        if value < 0 
+            value = 0
+        elsif value > 10
+            value = 10
+        end
+        @happiness = value 
+    end
+
+    def hygiene=(value)
+        if value > 10
+            value = 10
+        elsif value < 0
+            value = 0
+        end
+        @hygiene = value 
+    end
+
+    def clean? 
+        self.hygiene > 7
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount 
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -=2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else 
+            "blah blah blah blah blah"
+        end
+    end
+
+end
+
+
+person1 = Person.new("Brooke")
+person2 = Person.new("David")
+person3 = Person.new("Eleanor")
+
+# binding.pry
